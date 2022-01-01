ce21e259102e1cfe441399b56c013a154e5f1d15
Neil Kistner <neil.kistner@gmail.com>
2022-01-01 10:13:35 -0600

    Add simple !floor command

---
+++ src/commands/floor.ts
@@ -0,0 +1,59 @@
+import * as server from "../lib/server.js";
+import { Discord, SimpleCommand, SimpleCommandMessage } from "discordx";
+import { GraphQLClient } from "graphql-request";
+import { getSdk } from "../../generated/marketplace.graphql.js";
+
+const client = getSdk(new GraphQLClient(`${process.env.MARKETPLACE_URL}`));
+
+@Discord()
+abstract class Floor {
+  @SimpleCommand("floor")
+  async floor(command: SimpleCommandMessage) {
+    if (!server.isSmolBrains(command.message.guildId)) {
+      return;
+    }
+
+    if ("917430194049007616" !== command.message.channelId) {
+      return;
+    }
+
+    const message = command.message;
+
+    await message.channel.sendTyping();
+
+    try {
+      const floors = await client.getFloorPrices();
+
+      const female = floors.female[0].pricePerItem;
+      const land = floors.land[0].pricePerItem;
+      const male = floors.male[0].pricePerItem;
+      const vroom = floors.vroom[0].pricePerItem;
+
+      const fields = [
+        ["Male", male],
+        ["Female", female],
+        ["Land", land],
+        ["Vroom", vroom],
+      ].map(([name, price]) => ({
+        inline: true,
+        name,
+        value: `${(price / 1e18).toLocaleString()} $MAGIC`,
+      }));
+
+      await command.message.channel.send({
+        embeds: [
+          {
+            title: "Floor Price\n\u200b",
+            description: "",
+            color: 0x7e22ce,
+            fields,
+          },
+        ],
+      });
+    } catch (error) {
+      console.log(error);
+
+      await command.message.channel.send("Error fetching information.");
+    }
+  }
+}
--- src/graphql/marketplace/queries.graphql.ts
+++ src/graphql/marketplace/queries.graphql.ts
@@ -31,3 +31,50 @@ export const getSmolDetails = gql`
     }
   }
 `;
+
+export const getFloorPrices = gql`
+  query getFloorPrices {
+    female: listings(
+      first: 1
+      where: {
+        collection: "0x6325439389e0797ab35752b4f43a14c004f22a9c"
+        filters_contains: ["Gender,female"]
+        status: Active
+      }
+      orderBy: pricePerItem
+    ) {
+      pricePerItem
+    }
+    male: listings(
+      first: 1
+      where: {
+        collection: "0x6325439389e0797ab35752b4f43a14c004f22a9c"
+        filters_contains: ["Gender,male"]
+        status: Active
+      }
+      orderBy: pricePerItem
+    ) {
+      pricePerItem
+    }
+    land: listings(
+      first: 1
+      where: {
+        collection: "0xd666d1cc3102cd03e07794a61e5f4333b4239f53"
+        status: Active
+      }
+      orderBy: pricePerItem
+    ) {
+      pricePerItem
+    }
+    vroom: listings(
+      first: 1
+      where: {
+        collection: "0xb16966dad2b5a5282b99846b23dcdf8c47b6132c"
+        status: Active
+      }
+      orderBy: pricePerItem
+    ) {
+      pricePerItem
+    }
+  }
+`;
