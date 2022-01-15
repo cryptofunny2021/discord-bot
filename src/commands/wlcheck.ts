35cd3ffe7bedfb1da526d7fb60d20b4e5886e1a0
Neil Kistner <neil.kistner@gmail.com>
2022-01-15 17:07:13 -0600

    Switch Toadstoolz back to SimpleCommand

--- src/commands/wlcheck.ts
+++ src/commands/wlcheck.ts
@@ -1,4 +1,5 @@
 import * as server from "../lib/server.js";
+import * as sheets from "../lib/sheets.js";
 import { Discord, SimpleCommand, SimpleCommandMessage } from "discordx";
 
 @Discord()
@@ -6,12 +7,24 @@ abstract class WLCheck {
   @SimpleCommand("wlcheck")
   async wlcheck(command: SimpleCommandMessage) {
     const message = command.message;
-    const { channelId, guildId } = message;
+    const { channelId, content, guildId } = message;
+    const [, argument] = content.split(" ");
 
     try {
+      const wallet = argument.trim().toLowerCase();
+
       if (server.isEnjoyor(guildId) && channelId === "926595545928192041") {
         await message.reply(
           "The !wlcheck command is deprecated, use /wlcheck."
+        );
+      } else if (
+        server.isToadstoolz(guildId) &&
+        channelId === "929515043236757544"
+      ) {
+        await message.react(
+          sheets.toadstoolz().some((item) => item.endsWith(wallet))
+            ? "✅"
+            : "🚫"
         );
       }
     } catch (error) {
--- src/interactions/wlcheck.ts
+++ src/interactions/wlcheck.ts
@@ -1,12 +1,11 @@
 import * as server from "../lib/server.js";
 import * as sheets from "../lib/sheets.js";
-import * as user from "../lib/user.js";
 import { CommandInteraction } from "discord.js";
-import { Discord, Guild, Permission, Slash, SlashOption } from "discordx";
+import { Discord, Guild, Slash, SlashOption } from "discordx";
 
 @Discord()
 @Guild(server.ENJOYOR)
-class Enjoyor {
+class WLCheck {
   @Slash("wlcheck")
   async wlcheck(
     @SlashOption("wallet", {
@@ -25,37 +24,7 @@ class Enjoyor {
         ephemeral: true,
       });
     } catch (error) {
-      console.log("[enjoyor] /wlcheck error: ", error);
-    }
-  }
-}
-
-@Discord()
-@Guild(server.TOADSTOOLZ)
-class Toadstoolz {
-  @Permission(false)
-  @Permission({
-    id: user.ORANJE,
-    type: "USER",
-    permission: true,
-  })
-  @Slash("wlcheck")
-  async wlcheck(
-    @SlashOption("wallet", {
-      description: "The wallet to check against the whitelist.",
-    })
-    wallet: string,
-    interaction: CommandInteraction
-  ) {
-    try {
-      const found = sheets.toadstoolz().some((item) => item.endsWith(wallet));
-
-      await interaction.reply({
-        content: `That wallet is ${found ? "" : "not "}on the whitelist.`,
-        ephemeral: true,
-      });
-    } catch (error) {
-      console.log("[toadstoolz] /wlcheck error: ", error);
+      console.log("/wlcheck error: ", error);
     }
   }
 }
