7ee9fb1a739fe967bc39ff8b485348f9fee07195
Neil Kistner <neil.kistner@gmail.com>
2023-05-18 13:46:17 -0500

    Add !vee command

---
+++ src/commands/vee.ts
@@ -0,0 +1,80 @@
+import { formatDistanceToNowStrict } from 'date-fns'
+import { Discord, SimpleCommand, SimpleCommandMessage } from 'discordx'
+import { snapshot } from 'valtio/vanilla'
+
+import state from '../lib/vee.js'
+
+const spacer = { inline: true, name: '\u200b', value: '\u200b' }
+
+const TOKEN_LOGO = 'https://magicswap.lol/img/tokens/vee.png'
+
+@Discord()
+export class Vee {
+  @SimpleCommand()
+  async vee(command: SimpleCommandMessage) {
+    const { message } = command
+    const { channelId } = message
+    const vee = snapshot(state)
+
+    if (channelId === '882872974972162118') {
+      return
+    }
+
+    try {
+      await message.channel.sendTyping()
+
+      const fields = [
+        {
+          inline: true,
+          name: 'Price',
+          value: vee.price,
+        },
+        vee.change24h
+          ? [
+              spacer,
+              {
+                inline: true,
+                name: '24 Hour Change',
+                value: vee.change24h,
+              },
+            ]
+          : [],
+        vee.lastBuyPrice
+          ? [
+              {
+                name: 'Last Buy Price',
+                value: vee.lastBuyPrice,
+              },
+            ]
+          : [],
+      ]
+        .flat()
+        .filter(Boolean)
+
+      await command.message.channel.send({
+        embeds: [
+          {
+            title: '$VEE',
+            description: `- View on [DexScreener](https://dexscreener.com/arbitrum/${vee.pairId})\n- Trade on [MagicSwap](https://magicswap.lol/?input=MAGIC&output=VEE)`,
+            color: 0xe47454,
+            fields,
+            thumbnail: {
+              url: TOKEN_LOGO,
+              height: 64,
+              width: 64,
+            },
+            footer: {
+              text: `Powered by MagicSwap • ${formatDistanceToNowStrict(
+                vee.timestamp,
+                { addSuffix: true }
+              )}`,
+              icon_url: 'https://magicswap.lol/img/favicon-32x32.png',
+            },
+          },
+        ],
+      })
+    } catch (error) {
+      console.log('!vee Error: ', error)
+    }
+  }
+}
---
+++ src/lib/vee.ts
@@ -0,0 +1,63 @@
+import { proxy, snapshot } from 'valtio/vanilla'
+
+import coingecko from './coingecko.js'
+import dexscreener from './dexscreener.js'
+import { round } from './helpers.js'
+import magic from './magic.js'
+import pairs from './pairs.js'
+
+const state = proxy({
+  change24h: '',
+  lastBuyPrice: '',
+  pairId: '',
+  price: '',
+  timestamp: 0,
+  tokenId: '',
+})
+
+async function fetch() {
+  try {
+    const pair = snapshot(pairs).pairs.find((pair) => pair.name === 'VEE-MAGIC')
+    const { priceRaw } = snapshot(magic)
+
+    if (!pair) {
+      return
+    }
+
+    state.pairId = pair.id
+    state.tokenId = pair.token0.id
+
+    const info = snapshot(dexscreener).tokens.find(
+      (info) => info.id.toLowerCase() === pair.id.toLowerCase()
+    )
+
+    if (info) {
+      state.lastBuyPrice = info.priceUsd
+    }
+
+    const data = snapshot(coingecko).tokens.find(
+      (item) => item.id.toLowerCase() === state.tokenId.toLowerCase()
+    )
+
+    if (data) {
+      state.change24h = data.change24h
+    }
+
+    state.price = round(parseFloat(pair.token0.derivedETH) * priceRaw)
+    state.timestamp = Date.now()
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(`Error fetching VEE price\n${error.stack}`)
+    }
+  }
+}
+
+const { timestamp } = snapshot(state)
+
+// Refresh every 5 seconds
+if (timestamp === 0) {
+  fetch()
+  setInterval(fetch, 5_000)
+}
+
+export default state
